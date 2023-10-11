a936f505859e5bce9c098a3f0d3ee1a33405a37a
Harsh Goyal <goyalh862@gmail.com>
2023-10-11 19:19:16 +0530

    adding userData

---
+++ src/data/userData.js
@@ -0,0 +1,16 @@
+export const userData = [
+    {
+        name:"Harsh Goyal",
+        email:"goyalh9539@gmail.com",
+        password:"12345",
+        phone:"9785698756",
+        role:"admin"
+    },
+    {
+        name:"Harmanjot Kaur",
+        email:"harmanjotkaur@gmail.com",
+        password:"12345",
+        phone:"999999999",
+        role:"admin"
+    }
+]
