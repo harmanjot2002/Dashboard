56c307556adf0b60b13f83c4e42c2b5514334d2e
Harsh Goyal <goyalh862@gmail.com>
2023-10-23 15:39:58 +0530

    ProfileCard updated

--- src/scenes/teacher/components/ProfileCard.jsx
+++ src/scenes/teacher/components/ProfileCard.jsx
@@ -1,19 +1,16 @@
 import { Avatar, Button, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import web from "../../../assets/web.png";
 import { useNavigate } from "react-router-dom";
 import DeleteModal from "../../../components/DeleteModal";
 
 import {
     BarChart,
     Bar,
-    Cell,
     XAxis,
     YAxis,
     CartesianGrid,
     Tooltip,
     Legend,
-    ResponsiveContainer,
 } from "recharts";
 
 const weekData = [
@@ -51,103 +48,101 @@ const weekData = [
 
 const monthData = [
     {
-        name:"Week 1",
+        name: "Week 1",
         pv: 3,
         amt: 2400,
     },
     {
-        name:"Week 2",
+        name: "Week 2",
         pv: 4,
         amt: 2210,
     },
     {
-        name:"Week 3",
+        name: "Week 3",
         pv: 3.5,
         amt: 2290,
     },
     {
-        name:"Week 4",
+        name: "Week 4",
         pv: 4.2,
         amt: 2000,
     },
     {
-        name:"Week 5",
+        name: "Week 5",
         pv: 4.5,
         amt: 2181,
-    }
-]
+    },
+];
 const yearData = [
     {
-        name:"Jan",
+        name: "Jan",
         pv: 3,
         amt: 2400,
     },
     {
-        name:"Feb",
+        name: "Feb",
         pv: 4,
         amt: 2210,
     },
     {
-        name:"Mar",
+        name: "Mar",
         pv: 3.5,
         amt: 2290,
     },
     {
-        name:"Apr",
+        name: "Apr",
         pv: 4.2,
         amt: 2000,
     },
     {
-        name:"May",
+        name: "May",
         pv: 4.5,
         amt: 2181,
     },
     {
-        name:"Jun",
+        name: "Jun",
         pv: 3.1,
         amt: 2500,
     },
     {
-        name:"July",
+        name: "July",
         pv: 4.48,
         amt: 2400,
     },
     {
-        name:"Aug",
+        name: "Aug",
         pv: 3.26,
         amt: 2210,
     },
     {
-        name:"Sep",
+        name: "Sep",
         pv: 2.234,
         amt: 2290,
     },
     {
-        name:"Oct",
+        name: "Oct",
         pv: 3.7,
         amt: 2000,
     },
     {
-        name:"Nov",
+        name: "Nov",
         pv: 3.93,
         amt: 2181,
     },
     {
-        name:"Dec",
+        name: "Dec",
         pv: 4.87,
         amt: 2500,
-    }
-]
-
-
+    },
+];
 
 const ProfileCard = ({ teacher }) => {
     const navigate = useNavigate();
     const [isDelOpen, setIsDelOpen] = useState(false);
     const [delId, setDelId] = useState(null);
     const [delEmail, setDelEmail] = useState(null);
-    const [textVariant,setTextVariant] = useState("h4");
-    const [graphWidth,setGraphWidth] = useState(600);
+    const [textVariant, setTextVariant] = useState("h4");
+    const [graphWidth, setGraphWidth] = useState(600);
     const handleClose = () => {
         setIsDelOpen(false);
     };
@@ -157,10 +152,10 @@ const ProfileCard = ({ teacher }) => {
         setDelEmail(e.email);
     };
 
-    const [graphData,setGraphData] = useState(weekData);
+    const [graphData, setGraphData] = useState(weekData);
 
     const handleChange = (e) => {
-        switch(e.target.value){
+        switch (e.target.value) {
             case "week":
                 setGraphData(weekData);
                 break;
@@ -178,16 +173,16 @@ const ProfileCard = ({ teacher }) => {
 
     const token = JSON.parse(localStorage.getItem("token"));
 
-    useEffect(()=>{
-      if(window.innerWidth < 780 && window.innerWidth > 590){
-        setGraphWidth(500);
-        setTextVariant("h4");
-      }
-      if(window.innerWidth < 590){
-        setGraphWidth(300);
-        setTextVariant("h5");
-      }
-    },[])
+    useEffect(() => {
+        if (window.innerWidth < 780 && window.innerWidth > 590) {
+            setGraphWidth(500);
+            setTextVariant("h4");
+        }
+        if (window.innerWidth < 590) {
+            setGraphWidth(300);
+            setTextVariant("h5");
+        }
+    }, []);
 
     return (
         <div className="min-h-96 p-5 flex flex-col xl:flex-row justify-between items-center gap-10 w-full bg-slate-400 rounded-lg hover-effect ">
@@ -200,46 +195,62 @@ const ProfileCard = ({ teacher }) => {
                 <div className="flex flex-col justify-center md:justify-start items-center md:items-start">
                     <div className="flex gap-2">
                         <Typography variant={textVariant}>Name : </Typography>
-                        <Typography variant={textVariant}>{teacher.name}</Typography>
+                        <Typography variant={textVariant}>
+                            {teacher.name}
+                        </Typography>
                     </div>
                     <div className="flex gap-2">
                         <Typography variant={textVariant}>Age : </Typography>
-                        <Typography variant={textVariant}>{teacher.age}</Typography>
+                        <Typography variant={textVariant}>
+                            {teacher.age}
+                        </Typography>
                     </div>
                     <div className="flex gap-2">
                         <Typography variant={textVariant}>Groups : </Typography>
-                        <Typography variant={textVariant}>{teacher.group}</Typography>
+                        <Typography variant={textVariant}>
+                            {teacher.group}
+                        </Typography>
                     </div>
                     <div className="flex gap-2">
-                        <Typography variant={textVariant}>Subject : </Typography>
-                        <Typography variant={textVariant}>{teacher.subject}</Typography>
+                        <Typography variant={textVariant}>
+                            Subject :{" "}
+                        </Typography>
+                        <Typography variant={textVariant}>
+                            {teacher.subject}
+                        </Typography>
                     </div>
                     <div className="flex gap-2">
-                        <Typography variant={textVariant}>Lecture : </Typography>
-                        <Typography variant={textVariant}>{teacher.lecture}</Typography>
+                        <Typography variant={textVariant}>
+                            Lecture :{" "}
+                        </Typography>
+                        <Typography variant={textVariant}>
+                            {teacher.lecture}
+                        </Typography>
                     </div>
-                    { token?.role === 'admin' &&  <div className="flex gap-5 my-5">
-                        <Button
-                            color="secondary"
-                            variant="contained"
-                            onClick={() => {
-                                navigate("/update/teacher/" + teacher.id, {
-                                    state: teacher,
-                                });
-                            }}
-                        >
-                            Update
-                        </Button>
-                        <Button
-                            color="secondary"
-                            variant="contained"
-                            onClick={() => {
-                                handleOpen(teacher);
-                            }}
-                        >
-                            Delete
-                        </Button>
-                    </div>}
+                    {token?.role === "admin" && (
+                        <div className="flex gap-5 my-5">
+                            <Button
+                                color="secondary"
+                                variant="contained"
+                                onClick={() => {
+                                    navigate("/update/teacher/" + teacher.id, {
+                                        state: teacher,
+                                    });
+                                }}
+                            >
+                                Update
+                            </Button>
+                            <Button
+                                color="secondary"
+                                variant="contained"
+                                onClick={() => {
+                                    handleOpen(teacher);
+                                }}
+                            >
+                                Delete
+                            </Button>
+                        </div>
+                    )}
                 </div>
                 <DeleteModal
                     delId={delId}
@@ -252,29 +263,34 @@ const ProfileCard = ({ teacher }) => {
             <div className="flex-1 flex gap-5 flex-col justify-start xl:justify-center items-center xl:items-end w-full">
                 {/* <img className="h-80 w-80 " src={web} alt="web" /> */}
                 {/* <ResponsiveContainer width="100%" height="100%"> */}
-                <select onChange={handleChange} className="text-black p-2" name="data" id="data">
+                <select
+                    onChange={handleChange}
+                    className="text-black p-2"
+                    name="data"
+                    id="data"
+                >
                     <option value="week">Weekly</option>
                     <option value="month">month</option>
                     <option value="year">year</option>
                 </select>
-                    <BarChart
-                        width={graphWidth}
-                        height={300}
-                        data={graphData}
-                        margin={{
-                            top: 5,
-                            right: 30,
-                            // left: 20,
-                            bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="pv" fill="#8884d8" />
-                    </BarChart>
+                <BarChart
+                    width={graphWidth}
+                    height={300}
+                    data={graphData}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        // left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="pv" fill="#8884d8" />
+                </BarChart>
                 {/* </ResponsiveContainer> */}
             </div>
         </div>
